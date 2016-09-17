5b6d52c62f91557b0aa155862532a7dd7b8dace0
jose luis sanchez <muallin@gmail.com>
2016-09-17 11:01:32 +0200

    Initial Commit

---
+++ iDry/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  iDry
+//
+//  Created by jose luis sanchez on 17/9/16.
+//  Copyright © 2016 jose luis sanchez. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
